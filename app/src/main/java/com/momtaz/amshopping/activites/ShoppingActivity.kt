5bd84819aac407dc6d3210fc82c3bc47c7856261
Abdelrhman Momtaz <140316365+Abdelrhmanmomtaz58@users.noreply.github.com>
2024-07-07 20:50:14 +0300

    first commit

---
+++ app/src/main/java/com/momtaz/amshopping/activites/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package com.momtaz.amshopping.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.momtaz.amshopping.R
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
