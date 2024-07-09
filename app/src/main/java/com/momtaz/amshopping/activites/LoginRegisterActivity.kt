f61e52fded1bb5f3266f6a1611cff5561b9fc9ad
Abdelrhman Momtaz <140316365+Abdelrhmanmomtaz58@users.noreply.github.com>
2024-07-09 09:14:41 +0300

    no

--- app/src/main/java/com/momtaz/amshopping/activites/LoginRegisterActivity.kt
+++ app/src/main/java/com/momtaz/amshopping/activites/LoginRegisterActivity.kt
@@ -10,6 +10,5 @@ class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_register)
-        //
     }
 }
