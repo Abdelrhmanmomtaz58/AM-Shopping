7a61a1423b1c5cfb19d836b952b1e93e11716e34
Abdelrhman Momtaz <140316365+Abdelrhmanmomtaz58@users.noreply.github.com>
2024-07-09 05:16:00 +0300

    Introduction viewmodel

--- app/src/main/java/com/momtaz/amshopping/di/AppModule.kt
+++ app/src/main/java/com/momtaz/amshopping/di/AppModule.kt
@@ -1,8 +1,11 @@
 package com.momtaz.amshopping.di
 
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
+import com.momtaz.amshopping.util.Constants.INTRODUCTION_SP
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,4 +22,10 @@ object AppModule {
     @Provides
     @Singleton
     fun provideFirebaseFirestoreDatabase()= Firebase.firestore
+
+    @Provides
+    fun provideIntroductionSP(
+        application: Application
+
+    ) = application.getSharedPreferences(INTRODUCTION_SP, MODE_PRIVATE)
 }
--- app/src/main/java/com/momtaz/amshopping/util/Constants.kt
+++ app/src/main/java/com/momtaz/amshopping/util/Constants.kt
@@ -2,5 +2,7 @@ package com.momtaz.amshopping.util
 
 object Constants {
     const val USER_COLLECTION = "user"
+    const val INTRODUCTION_SP = "introductionSP"
+    const val INTRODUCTION_KEY = "IntroductionKey"
 
 }
