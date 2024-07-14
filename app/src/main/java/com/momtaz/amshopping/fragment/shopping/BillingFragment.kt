e0dfcdbf40fb0cca141cbdb741e787cfa0cda956
Abdelrhman Momtaz <140316365+Abdelrhmanmomtaz58@users.noreply.github.com>
2024-07-14 03:09:25 +0300

    Place Order

--- app/src/main/java/com/momtaz/amshopping/fragment/shopping/BillingFragment.kt
+++ app/src/main/java/com/momtaz/amshopping/fragment/shopping/BillingFragment.kt
@@ -1,5 +1,6 @@
 package com.momtaz.amshopping.fragment.shopping
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,15 +13,19 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.momtaz.amshopping.R
 import com.momtaz.amshopping.adapters.AddressAdapter
 import com.momtaz.amshopping.adapters.BillingProductsAdapter
+import com.momtaz.amshopping.data.Address
 import com.momtaz.amshopping.data.CartProduct
-import com.momtaz.amshopping.databinding.FragmentBaseCategoryBinding
+import com.momtaz.amshopping.data.order.Order
+import com.momtaz.amshopping.data.order.OrderStatus
 import com.momtaz.amshopping.databinding.FragmentBillingBinding
 import com.momtaz.amshopping.util.HorizontalItemDecoration
 import com.momtaz.amshopping.util.Resource
 import com.momtaz.amshopping.viewmodel.BillingViewModel
+import com.momtaz.amshopping.viewmodel.OrderViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 @AndroidEntryPoint
@@ -28,10 +33,12 @@ class BillingFragment : Fragment() {
     private lateinit var binding: FragmentBillingBinding
     private val addressAdapter by lazy { AddressAdapter() }
     private val billingProductsAdapter by lazy { BillingProductsAdapter() }
-    private val viewModel by viewModels<BillingViewModel>()
+    private val billingViewModel by viewModels<BillingViewModel>()
     private val args by navArgs<BillingFragmentArgs>()
     private var products = emptyList<CartProduct>()
     private var totalPrice = 0f
+    private var selectedAddress: Address? =null
+    private val orderViewModel by viewModels<OrderViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,7 +64,7 @@ class BillingFragment : Fragment() {
             findNavController().navigate(R.id.action_billingFragment_to_addressFragment)
         }
         lifecycleScope.launchWhenStarted {
-            viewModel.address.collectLatest {
+            billingViewModel.address.collectLatest {
                 when(it){
                     is Resource.Loading ->{
                         binding.progressbarAddress.visibility =View.VISIBLE
@@ -74,8 +81,66 @@ class BillingFragment : Fragment() {
                 }
             }
         }
+
+        lifecycleScope.launchWhenStarted {
+            orderViewModel.oder.collectLatest {
+                when(it){
+                    is Resource.Loading ->{
+                        binding.buttonPlaceOrder.startAnimation()
+                    }
+                    is Resource.Success ->{
+                        binding.buttonPlaceOrder.revertAnimation()
+                        findNavController().navigateUp()
+                        Snackbar.make(requireView(),"Your order was placed",Snackbar.LENGTH_LONG).show()
+                    }
+                    is Resource.Error ->{
+                        binding.buttonPlaceOrder.revertAnimation()
+                        Toast.makeText(requireContext(),it.message,Toast.LENGTH_LONG).show()
+
+                    }else -> Unit
+                }
+            }
+        }
+
         billingProductsAdapter.differ.submitList(products)
         binding.tvTotalPrice.text= "$ $totalPrice"
+
+        addressAdapter.onClick = {
+            selectedAddress = it
+        }
+
+        binding.buttonPlaceOrder.setOnClickListener {
+            if (null == selectedAddress){
+                Toast.makeText(requireContext(),"Please select an address",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            showOrderConfirmationDialog()
+        }
+        binding.imageCloseBilling.setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
+    private fun showOrderConfirmationDialog() {
+        val alertDialog = AlertDialog.Builder(requireContext()).apply {
+            setTitle("Order items")
+            setMessage("Do you want to order your cart items?")
+            setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            setPositiveButton("Yes") { dialog, _ ->
+                val order = Order(
+                    OrderStatus.Ordered.status,
+                    totalPrice,
+                    products,
+                    selectedAddress!!
+                )
+                orderViewModel.placeOrder(order)
+                dialog.dismiss()
+            }
+        }
+        alertDialog.create()
+        alertDialog.show()
     }
 
     private fun setupAddressRv() {
---
+++ app/src/main/java/com/momtaz/amshopping/viewmodel/OrderViewModel.kt
@@ -0,0 +1,57 @@
+package com.momtaz.amshopping.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.momtaz.amshopping.data.order.Order
+import com.momtaz.amshopping.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OrderViewModel @Inject constructor(
+    private val firestore: FirebaseFirestore,
+    private val auth: FirebaseAuth
+) : ViewModel() {
+
+    private val _oder = MutableStateFlow<Resource<Order>>(Resource.Unspecified())
+    val oder = _oder.asStateFlow()
+
+    fun placeOrder(oder: Order) {
+        viewModelScope.launch {
+            _oder.emit(Resource.Loading())
+        }
+        firestore.runBatch {batch ->
+            //TODO: ADD the oder into user orders collection
+            //TODO: add the oder into orders collection
+            //TODO: Delete the product from user cart collection
+
+            firestore.collection("user")
+                .document(auth.uid!!)
+                .collection("orders")
+                .document()
+                .set(oder)
+
+            firestore.collection("orders")
+                .document().set(oder)
+            firestore.collection("user").document(auth.uid!!).collection("cart").get()
+                .addOnSuccessListener {
+                    it.documents.forEach{
+                        it.reference.delete()
+                    }
+                }
+        }.addOnSuccessListener {
+            viewModelScope.launch {
+                _oder.emit(Resource.Success(oder))
+            }
+        }.addOnFailureListener {
+            viewModelScope.launch {
+                _oder.emit(Resource.Error(it.message.toString()))
+            }
+        }
+    }
+}
